2fde600cb1092f2611ca88aa014f9ed73dfc734c
aBitNomadic <aBitNomadic@users.noreply.github.com>
2020-08-02 21:13:34 -0400

    Update start.sh
    no comment

--- start.sh
+++ start.sh
@@ -16,9 +16,8 @@ echo Server starting
 if [ ! -z "$SPW" ]
 then
   #im not proud of this, if anyones got a better idea please let me know
-  /opt/mumble/murmur.x86 -fg -ini /opt/mumble/data/murmur.ini &
-  /opt/mumble/murmur.x86 -supw "$SPW" &
-  fg %-
+  /opt/mumble/murmur.x86 -fg -ini /opt/mumble/data/murmur.ini;
+  /opt/mumble/murmur.x86 -supw "$SPW"
 else
   /opt/mumble/murmur.x86 -fg -ini /opt/mumble/data/murmur.ini
 fi
