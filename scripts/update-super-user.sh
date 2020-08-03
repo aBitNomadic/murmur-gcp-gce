f3914f0fe2544c2e3060c47c33277d3ee10f3c03
aBitNomadic <aBitNomadic@users.noreply.github.com>
2020-08-02 22:01:20 -0400

    Update update-super-user.sh
    Please stop fighting me mumble

--- scripts/update-super-user.sh
+++ scripts/update-super-user.sh
@@ -7,5 +7,8 @@ until pids=$(pidof murmur.x86)
 do   
     sleep 1
 done
+#I just dont have a better answer
+echo "Done waiting for murmur to start, Sleep for a few seconds to let it get set up"
+sleep 3
 
 /opt/mumble/murmur.x86 -supw "$SPW"
